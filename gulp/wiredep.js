46143b0ebff4ac1d5b8be22cb71a985038cd5181
Sten Hägglund <sten.hagglund@gmail.com>
2014-12-04 21:08:45 +0200

    Override bower components, use locale fi

--- gulp/wiredep.js
+++ gulp/wiredep.js
@@ -9,6 +9,22 @@ gulp.task('wiredep', function () {
   return gulp.src('src/index.html')
     .pipe(wiredep({
       directory: 'bower_components',
+      overrides: {
+        'components-font-awesome': {
+          main: [
+            'css/font-awesome.css',
+            'fonts/fontawesome-webfont.eot',
+            'fonts/fontawesome-webfont.svg',
+            'fonts/fontawesome-webfont.ttf',
+            'fonts/fontawesome-webfont.woff'
+          ]
+        },
+        'angular-i18n': {
+          main: [
+            'angular-locale_fi-fi.js'
+          ]
+        }
+      },
       exclude: [/bootstrap-sass-official/, /bootstrap.js/, /bootstrap.css/],
     }))
     .pipe(gulp.dest('src'));
